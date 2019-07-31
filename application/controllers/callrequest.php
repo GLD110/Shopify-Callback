ecd72f7f3213391bb8b81c49f868a39d1fed9e7b
diana4545 <diana4545@outlook.com>
2019-07-31 20:12:19 +0100

    Update callrequest.php

--- application/controllers/callrequest.php
+++ application/controllers/callrequest.php
@@ -25,27 +25,27 @@ class Callrequest extends MY_Controller {
   }
 
   public function addCallrequest(){
-    // $this->load->model('Callrequest_model');
-    // $this->load->model('Integration_model');
-    // $this->load->model('Settings_model');
-    //
-    // $request = $this->input->post();
-    //
-    // $emails = $this->Integration_model->getEmails();
-    // $call_type = $request['call_type'];
-    //
-    // if($call_type == 'general')
-    //   $service_email = $emails->result()[0]->generalEmail;
-    // if($call_type == 'sales')
-    //   $service_email = $emails->result()[0]->salesEmail;
-    // if($call_type == 'order')
-    //   $service_email = $emails->result()[0]->orderEmail;
-    // if($call_type == 'corporate')
-    //   $service_email = $emails->result()[0]->coporateEmail;
-    // if($call_type == 'complaint')
-    //   $service_email = $emails->result()[0]->complaintEmail;
-    // if($call_type == 'other')
-    //   $service_email = $emails->result()[0]->otherEmail;
+    $this->load->model('Callrequest_model');
+    $this->load->model('Integration_model');
+    $this->load->model('Settings_model');
+
+    $request = $this->input->post();
+
+    $emails = $this->Integration_model->getEmails();
+    $call_type = $request['call_type'];
+
+    if($call_type == 'general')
+      $service_email = $emails->result()[0]->generalEmail;
+    if($call_type == 'sales')
+      $service_email = $emails->result()[0]->salesEmail;
+    if($call_type == 'order')
+      $service_email = $emails->result()[0]->orderEmail;
+    if($call_type == 'corporate')
+      $service_email = $emails->result()[0]->coporateEmail;
+    if($call_type == 'complaint')
+      $service_email = $emails->result()[0]->complaintEmail;
+    if($call_type == 'other')
+      $service_email = $emails->result()[0]->otherEmail;
 
     $my_timezone =  $this->Settings_model->getList()->result()[0]->value_wgt_timezone;
     $delta_hours = str_replace('GMT', '', $my_timezone) - str_replace('GMT', '', $request['time_gmt']);
