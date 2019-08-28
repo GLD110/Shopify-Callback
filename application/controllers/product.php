1e079ae862651b96171af38fe3cab5759c4b0bf3
diana4545 <diana4545@outlook.com>
2019-08-29 00:16:49 +0100

    Update product.php

--- application/controllers/product.php
+++ application/controllers/product.php
@@ -23,36 +23,36 @@ class Product extends MY_Controller {
     $this->manage();
   }
 
-  public function manage( $page =  0 ){
-    // Check the login
-    $this->is_logged_in();
-
-    // Init the search value
-    $this->initSearchValue();
-
-    // Get data
-    $arrCondition =  array(
-         'name' => $this->_searchVal['name'],
-         'sort' => $this->_searchVal['sort_field'] . ' ' . $this->_searchVal['sort_direction'],
-         'page_number' => $page,
-         'page_size' => $this->_searchVal['page_size'],
-    );
-    $data['query'] =  $this->Product_model->getList( $arrCondition );
-    $data['total_count'] = $this->Product_model->getTotalCount();
-    $data['page'] = $page;
-    $data['shop'] = $this->session->userdata('shop');
-
-    // Define the rendering data
-    $data = $data + $this->setRenderData();
-
-    // Load Pagenation
-    $this->load->library('pagination');
-    $this->load->library( 'LiquidLib' );
-
-    $this->load->view('view_header');
-    $this->load->view('view_product', $data );
-    $this->load->view('view_footer');
-  }
+  // public function manage( $page =  0 ){
+  //   // Check the login
+  //   $this->is_logged_in();
+  //
+  //   // Init the search value
+  //   $this->initSearchValue();
+  //
+  //   // Get data
+  //   $arrCondition =  array(
+  //        'name' => $this->_searchVal['name'],
+  //        'sort' => $this->_searchVal['sort_field'] . ' ' . $this->_searchVal['sort_direction'],
+  //        'page_number' => $page,
+  //        'page_size' => $this->_searchVal['page_size'],
+  //   );
+  //   $data['query'] =  $this->Product_model->getList( $arrCondition );
+  //   $data['total_count'] = $this->Product_model->getTotalCount();
+  //   $data['page'] = $page;
+  //   $data['shop'] = $this->session->userdata('shop');
+  //
+  //   // Define the rendering data
+  //   $data = $data + $this->setRenderData();
+  //
+  //   // Load Pagenation
+  //   $this->load->library('pagination');
+  //   $this->load->library( 'LiquidLib' );
+  //
+  //   $this->load->view('view_header');
+  //   $this->load->view('view_product', $data );
+  //   $this->load->view('view_footer');
+  // }
 
   public function sync( $page = 1 )
   {
